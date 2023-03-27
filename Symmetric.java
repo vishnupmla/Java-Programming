3d5a81f19a5138e9d8a3ef41d951b00e3ed64267
vishnupmla <116819304+vishnupmla@users.noreply.github.com>
2023-03-27 23:28:45 +0530

    Update code to check symmetric matrix

--- Symmetric.java
+++ Symmetric.java
@@ -1,65 +1,58 @@
 import java.util.Scanner;
 
-public class Symmetric
-{
-	public static void main(String args[])
-	{
-		Scanner sc = new Scanner(System.in);
-		int[][] matrix = new int[20][20];
-		int[][] transpose = new int[20][20];
-		int s,i,j,flag=0;
-		
-		System.out.println("Enter the size of matrix:");
-		
-		s= sc.nextInt();
-		System.out.println("Enter the Matrix");
-		for(i=0;i<s;i++)
-		{
-			for(j=0;j<s;j++)
-			{
-				matrix[i][j] = sc.nextInt();
-			}
-		}
+public class Symmetric {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int[][] matrix = new int[20][20];
+        int[][] transpose = new int[20][20];
+        int r, c, i, j, flag = 0;
 
-		System.out.println("The matrix is");
-		for(i=0;i<s;i++)
-		{
-			
-			for(j=0;j<s;j++)
-			{
-				System.out.print(matrix[i][j] +" ");
-			}
-			System.out.println();
-		}
+        System.out.println("Enter the rows of matrix:");
+        r = sc.nextInt();
+        System.out.println("Enter the columns of matrix:");
+        c = sc.nextInt();
 
-		for(i=0;i<s;i++)
-		{
-			for(j=0;j<s;j++)
-			{
-				transpose[j][i] = matrix[i][j];
-			}
-		}
-		
-		for(i=0;i<s;i++)
-		{
-			for(j=0;j<s;j++)
-			{
-				if(matrix[i][j] != transpose[i][j])
-				{
-				flag =1;
-				break;
-				}
-			}
-		}
-		
-		if(flag ==1 )
-		{
-			System.out.println("The matrix is not symmetric");
-		}
-		
-		else
-		{
-			System.out.println("The matrix is symmetric");
-		}
-	}
+        if (r == c) {
+            System.out.println("Enter the Matrix");
+            for (i = 0; i < r; i++) {
+                for (j = 0; j < c; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+
+            System.out.println("The matrix is");
+            for (i = 0; i < r; i++) {
+
+                for (j = 0; j < c; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+            for (i = 0; i < r; i++) {
+                for (j = 0; j < c; j++) {
+                    transpose[j][i] = matrix[i][j];
+                }
+            }
+
+            for (i = 0; i < r; i++) {
+                for (j = 0; j < c; j++) {
+                    if (matrix[i][j] != transpose[i][j]) {
+                        flag = 1;
+                        break;
+                    }
+                }
+            }
+
+            if (flag == 1) {
+                System.out.println("The matrix is not symmetric");
+            }
+
+            else {
+                System.out.println("The matrix is symmetric");
+            }
+        } else {
+            System.out.println("Unable to determine");
+        }
+    }
 }
